81f3773624481c126c3791be90e8f372845ca51e
이승환 <154510650+sonic8-8@users.noreply.github.com>
2024-08-22 14:39:22 +0900

    Feaute/s3-upload (#4)
    Amazon S3 Bucket에 파일 업로드하는 기능 개발

--- back-end/src/main/java/com/gongcha/berrymatch/s3bucket/S3Service.java
+++ back-end/src/main/java/com/gongcha/berrymatch/s3bucket/S3Service.java
@@ -1,14 +1,15 @@
 package com.gongcha.berrymatch.s3bucket;
 
-import com.gongcha.berrymatch.s3bucket.requestDTO.S3PostFileUploadServiceRequest;
-import com.gongcha.berrymatch.s3bucket.responseDTO.S3PostFileUploadResponse;
-import io.awspring.cloud.s3.S3Resource;
+import com.gongcha.berrymatch.ApiResponse;
+import com.gongcha.berrymatch.postFile.PostFile;
+import com.gongcha.berrymatch.postFile.requestDTO.PostFileUploadServiceRequest;
+import com.gongcha.berrymatch.postFile.responseDTO.PostFileUploadResponse;
 import io.awspring.cloud.s3.S3Template;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
-import software.amazon.awssdk.services.s3.S3Client;
+
 
 import java.io.IOException;
 import java.util.UUID;
@@ -19,14 +20,15 @@ public class S3Service {
 
     @Value("${AWS_S3_BUCKET_NAME}")
     private String bucketName;
-    private final S3Client s3Client;
+
     private final S3Template s3Template;
 
     /**
-     * S3 버킷에 영상을 업로드해주는 메서드
+     * S3 버킷에 하이라이트 영상을 업로드해주는 메서드
      */
-    public S3PostFileUploadResponse uploadVideo(S3PostFileUploadServiceRequest request) throws IOException {
+    public String uploadVideo(PostFileUploadServiceRequest request) throws IOException {
 
+        ValidateVideoFilter(request);
 
         MultipartFile file = request.getFile();
 
@@ -36,21 +38,38 @@ public class S3Service {
 
         s3Template.upload(bucketName, key, file.getInputStream());
 
-        S3PostFileUploadResponse.of()
+        return key;
 
+    }
 
-        return S3PostFileUploadResponse.of()
+    /**
+     * S3 버킷에 하이라이트 사진을 업로드해주는 메서드
+     */
+    public String uploadImage(PostFileUploadServiceRequest request) throws IOException {
+
+        ValidateVideoFilter(request);
+
+        MultipartFile file = request.getFile();
+
+        String directory = "highlight_video/";
+        String storedFilename = UUID.randomUUID().toString() + "_" + file.getOriginalFilename();
+        String key = directory + storedFilename;
+
+        s3Template.upload(bucketName, key, file.getInputStream());
+
+        return key;
 
     }
 
     /**
      * 영상 파일 크기와 타입을 검증해주는 메서드
      */
-    private static boolean isValidateVideo(S3PostFileUploadServiceRequest request) {
+    private static void ValidateVideoFilter(PostFileUploadServiceRequest request) {
         MultipartFile file = request.getFile();
 
         String contentType = file.getContentType();
-        if (contentType == null || !contentType.startsWith("Video/")) {
+        if (contentType == null || !contentType.startsWith("video/")) {
+            System.out.println(file.getContentType());
             throw new IllegalArgumentException("영상 파일만 업로드할 수 있습니다.");
         }
 
@@ -59,7 +78,25 @@ public class S3Service {
             throw new IllegalArgumentException("영상 파일 크기는 500MB를 초과할 수 없습니다.");
         }
 
-        return true;
+    }
+
+    /**
+     * 사진 파일 크기와 타입을 검증해주는 메서드
+     */
+    private static void ValidateImageFilter(PostFileUploadServiceRequest request) {
+        MultipartFile file = request.getFile();
+
+        String contentType = file.getContentType();
+        if (contentType == null || !contentType.startsWith("image/")) {
+            System.out.println(file.getContentType());
+            throw new IllegalArgumentException("사진 파일만 업로드할 수 있습니다.");
+        }
+
+        Long maxFileSize = 500 * 1024 * 1024L;
+        if (file.getSize() > maxFileSize) {
+            throw new IllegalArgumentException("사진 파일 크기는 500MB를 초과할 수 없습니다.");
+        }
+
     }
 
 }
--- back-end/src/test/java/com/gongcha/berrymatch/s3bucket/S3ServiceTest.java
+++ back-end/src/test/java/com/gongcha/berrymatch/s3bucket/S3ServiceTest.java
@@ -1,49 +1,55 @@
 package com.gongcha.berrymatch.s3bucket;
 
+import com.gongcha.berrymatch.postFile.requestDTO.PostFileUploadServiceRequest;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.mockito.BDDMockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.mock.web.MockMultipartFile;
 import org.springframework.test.context.ActiveProfiles;
-import org.springframework.web.multipart.MultipartFile;
-import software.amazon.awssdk.services.s3.model.PutObjectResponse;
+import software.amazon.awssdk.services.s3.S3Client;
 
+
+import java.io.File;
+import java.io.FileInputStream;
 import java.io.IOException;
-import java.io.InputStream;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.junit.jupiter.api.Assertions.*;
+
 
 @ActiveProfiles("test")
 @SpringBootTest
 class S3ServiceTest {
 
-    @Autowired
+    @MockBean
     private S3Service s3Service;
+
 
     @DisplayName("S3 Bucket에 영상을 업로드 할 수 있다.")
     @Test
     void uploadFile() throws IOException {
         // given
-        byte[] videoBytes = new byte[] {
-                0x00, 0x00, 0x00, 0x18, 0x66, 0x74, 0x79, 0x70, // MP4 파일 헤더 일부
-                0x6D, 0x70, 0x34, 0x32, 0x00, 0x00, 0x00, 0x00,
-                0x6D, 0x70, 0x34, 0x31
-        };
-
-        MultipartFile file = new MockMultipartFile(
-                "video",
-                "testvideo.mp4",
-                "video/mp4",
-                videoBytes
+        MockMultipartFile file = new MockMultipartFile(
+                "video",                      // 파일 이름
+                "test.mp4",                   // 원본 파일 이름
+                "video/mp4",                  // MIME 타입
+                new FileInputStream(new File("C:\\Users\\YOU\\Videos\\test_video.mp4"))                    // 파일 내용
         );
 
+        PostFileUploadServiceRequest request = PostFileUploadServiceRequest.builder()
+                .file(file)
+                .build();
+
+        BDDMockito.given(s3Service.uploadVideo(request)).willReturn("key");
+
         // when
-        String fileName = s3Service.uploadVideo(file);
+        String key = s3Service.uploadVideo(request);
 
         // then
-        assertThat(fileName).isNotNull();
+        assertThat(key).isNotNull()
+                .isEqualTo("key");
     }
 
 }
