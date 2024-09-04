9b83e4e3825fd1a3c93079419300092dab8a70c6
이승환 <seaung13@gmail.com>
2024-09-04 09:46:59 +0900

    마이 페이지 개발중

--- front-end/src/user/pages/ProfileEditSubPage.jsx
+++ front-end/src/user/pages/ProfileEditSubPage.jsx
@@ -1,10 +1,27 @@
+import useUserInfo from '../useUserInfo';
+import styles from './ProfileEditSubPage.module.css';
+
 function ProfileEditSubPage() {
 
+    const { userInfo, loading, error } = useUserInfo();
+
+    if (loading) {
+        return <div>로딩중...</div>;
+    }
+
+    if (error) {
+        return <div>Error: {error.message}</div>;
+    }
 
     return (
-        <div>
+        <div className={styles.profile}>
 
-
+            <div className={styles.profile_info}>
+                닉네임 : {userInfo.nickname}
+                도시 : {userInfo.city}
+                구 : {userInfo.district}
+                성별 : {userInfo.gender}
+            </div>
             
         </div>
     )
