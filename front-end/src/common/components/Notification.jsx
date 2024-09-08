a60151fb6a7dd216e1534b7b477e1946c8c232eb
이승환 <seaung13@gmail.com>
2024-09-08 21:21:46 +0900

    FE: Notification.jsx params 수정

--- front-end/src/common/components/Notification.jsx
+++ front-end/src/common/components/Notification.jsx
@@ -4,23 +4,33 @@ import Cookies from 'js-cookie';
 import { EventSourcePolyfill } from 'event-source-polyfill'; // 올바르게 임포트
 
 const Notification = () => {
+
     const [notifications, setNotifications] = useState([]);
     const accessToken = Cookies.get('accessToken');
 
     useEffect(() => {
+
+        const decodedToken = jwtDecode(accessToken);
+        const userId = decodedToken.id;
+
+        console.log(userId);
+
+        // 1번째 HTTP 통신에서만 인증이 필요함. 인증 후에는 연결이 유지됨.
         const eventSource = new EventSourcePolyfill('http://localhost:8085/api/stream', {
+            params: { 'userId': userId },
             headers: {
                 'Authorization': `Bearer ${accessToken}`,
                 'Accept': 'text/event-stream',
-                
             },
             withCredentials: true
         });
 
+        // 서버에서 메세지 받았을 경우
         eventSource.onmessage = (event) => {
             setNotifications((prevNotifications) => [...prevNotifications, event.data]);
         };
 
+        // 에러 발생 시 연결 끊기
         eventSource.onerror = (error) => {
             console.error('SSE 오류:', error);
             eventSource.close();
