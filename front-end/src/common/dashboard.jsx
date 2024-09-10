69c36e42f71fc2a880f8cc268bd8a8a0ebb12fdf
윤슬아 <seulayun1043@gmail.com>
2024-09-10 14:06:05 +0900

    메인 화면 css 수정 중

--- front-end/src/common/dashboard.jsx
+++ front-end/src/common/dashboard.jsx
@@ -25,11 +25,14 @@ function Dashboard() {
                     <img className={styles.dashboard_top_logo} src='https://thank-you-berrymatch-bucket-0.s3.ap-northeast-2.amazonaws.com/design/logo.png'/>
                     <div className={styles.dashboard_top_logo_title}>BerryMatch</div>
                 </div>
-                <div className={styles.dashboard_top_identifier}>환영합니다! {userInfo ? userInfo.nickname : '' }님</div>
-                <Link to="/logout" className={styles.dashboard_top_logout}>
-                        로그아웃
-                </Link>
-                {/* <Link to="/map">맵 테스트</Link> */}
+                <div className={styles.dashboard_top_user_container}>
+                    <div className={styles.dashboard_top_identifier}>환영합니다! {userInfo ? userInfo.nickname : '' }님</div>
+                    <Link to="/logout" className={styles.dashboard_top_logout}>
+                            로그아웃
+                    </Link>
+                    {/* <Link to="/map">맵 테스트</Link> */}
+
+                </div>
             </div>
 
             <div className={styles.dashboard_middle}>
@@ -46,17 +49,17 @@ function Dashboard() {
 
                 <div className={styles.dashboard_middle_right}>
                     <div className={styles.dashboard_middle_right_menu_container}>
-                        <div className={styles.dashboard_middle_right_menu}>
+                        <div className={styles.dashboard_middle_right_menu_large}>
                             <MatchStatus/>
                             <MatchSetupSubPage />
                         </div>
                     </div>
                     <div className={styles.dashboard_middle_right_menu_container}>
-                        <Link to="/rank" className={styles.dashboard_middle_right_menu}>랭킹</Link>
-                        <Link to="/board" className={styles.dashboard_middle_right_menu}>하이라이트 게시판</Link>
+                        <Link to="/rank" className={styles.dashboard_middle_right_menu_small}>랭킹</Link>
+                        <Link to="/board" className={styles.dashboard_middle_right_menu_small}>하이라이트</Link>
                     </div>
                     <div className={styles.dashboard_middle_right_menu_container}>
-                        <Link to="/guild" className={styles.dashboard_middle_right_menu}>길드</Link>
+                        <Link to="/guild" className={styles.dashboard_middle_right_menu_large}>길드</Link>
                         {/* <Link to="/group" className={styles.dashboard_middle_right_menu}>그룹찾기/그룹생성</Link> */}
                     </div>
                 </div>
